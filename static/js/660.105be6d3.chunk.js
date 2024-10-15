6768c673b6991dc7ab7d6e01bca1497ad545c191
Ijusttyped <60543555+Ijusttyped@users.noreply.github.com>
2024-10-15 07:34:51 +0000

    Deploying to gh-pages from @ Ijusttyped/dev-portfolio@b3e9237b471e57a761d5664632e70a80a7f7a205 🚀

---
+++ static/js/660.105be6d3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_portfolio=self.webpackChunkdev_portfolio||[]).push([[660],{7660:(e,t,s)=>{s.r(t),s.d(t,{default:()=>r});s(5043),s(4050);var o=s(579);const r=function(e){const{title:t}=e;return(0,o.jsx)("div",{className:"header",children:t})}}}]);
+//# sourceMappingURL=660.105be6d3.chunk.js.map
